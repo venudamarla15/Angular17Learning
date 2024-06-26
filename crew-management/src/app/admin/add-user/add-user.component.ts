27fc6c08f1f89deafe437900132734eb1c3bb9a4
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-06-25 23:10:26 -0500

    string interpolation

--- crew-management/src/app/admin/add-user/add-user.component.ts
+++ crew-management/src/app/admin/add-user/add-user.component.ts
@@ -9,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AddUserComponent {
 
+  // interpolation 
+  userName: string = '';
+  userId: number = 20;
+  name: string ='iphone 15 pro Max';
+  price: number = 999;
+  color: string [] = ['black', 'blue', 'orange'];
+
+  product = {
+    name : 'Iphone x',
+    price : 999 ,
+    color :  'Black',
+    discount : 8.95,
+    instock : 5
+  }
+
+  getDiscountPrice() {
+   return this.product.price -(this.product.price * 9.5/100)
+  }
+
 }
